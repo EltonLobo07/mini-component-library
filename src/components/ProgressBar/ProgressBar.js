3e9e16e0105d45ad8e9915922ba25006be94568d
Elton Lobo <eltonsw00@gmail.com>
2023-08-01 08:50:31 +0530

    Adjustments after watching the solution video

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -18,9 +18,15 @@ const PROGRESS_SIZE_VARS = {
 };
 
 const WRAPPER_SIZE_VARS = {
+  small: {
+    "--borderRadius": "4px"
+  },
+  medium: {
+    "--borderRadius": "4px"
+  },
   large: {
     "--padding": "4px",
-    "--borderRadius": "8px"
+    "--borderRadius": "12px"
   }
 };
 
@@ -29,22 +35,33 @@ const Wrapper = styled.div`
   width: 370px;
   background-color: ${COLORS.transparentGray15};
   padding: var(--padding, 0);
-  border-radius: var(--borderRadius, 4px);
+  border-radius: var(--borderRadius);
   box-shadow: inset 0 2px 4px 0 ${COLORS.transparentGray15};
   overflow: hidden;
+`;
+
+const ProgressWrapper = styled.div`
+  overflow: hidden;
+  border-radius: inherit;
 `;
 
 const Progress = styled.div`
   width: ${p => p.value}%;
   background-color: ${COLORS.primary};
   height: var(--height);
-  border-radius: 4px ${p => p.value === 100 ? "4px" : "0"} ${p => p.value === 100 ? "4px" : "0"} 4px;
+  border-radius: 4px 0 0 4px;
 `;
 
 const MIN_VALUE = 0;
 const MAX_VALUE = 100;
 
 const ProgressBar = ({ value, size, label = "progress bar" }) => {
+  const progressSizeVars = PROGRESS_SIZE_VARS[size];
+  const wrapperSizeVars = WRAPPER_SIZE_VARS[size];
+  if (!progressSizeVars || !wrapperSizeVars) {
+    throw new Error(`Unknown size value passed to ProgressBar: ${size}`);
+  }
+
   let currentValue;
   if (typeof value === "number") {
     currentValue = Math.max(Math.min(value, MAX_VALUE), 0);
@@ -59,10 +76,16 @@ const ProgressBar = ({ value, size, label = "progress bar" }) => {
       aria-label = {label}
       style = {WRAPPER_SIZE_VARS[size]}
     >
-      <Progress 
-        style = {PROGRESS_SIZE_VARS[size]}
-        value = {currentValue}
-      />
+      <ProgressWrapper>
+        <Progress 
+          style = {PROGRESS_SIZE_VARS[size]}
+          value = {currentValue}
+        >
+          <VisuallyHidden>
+            {currentValue}%
+          </VisuallyHidden>
+        </Progress>
+      </ProgressWrapper>
     </Wrapper>
   );
 };
