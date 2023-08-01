1212222554ec69325b1fdbd6242aefe91ed2c1d4
Elton Lobo <eltonsw00@gmail.com>
2023-08-01 10:37:25 +0530

    Add my "Select" component solution
    I wasn't able to figure out how how to keep the focus of the native
    select element visible. Apart from that, I think I have covered all
    of the other points mentioned in the Figma file.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -4,15 +4,64 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
+import VisuallyHidden from "../VisuallyHidden";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper 
+      value = {value} 
+      onChange = {onChange}
+    >
+      <SelectedWrapper>
+        <span>
+          {displayedValue}
+        </span>
+        <Icon 
+          id = "chevron-down"
+        />
+      </SelectedWrapper>
+      <label>
+        <VisuallyHidden>
+          {label}
+        </VisuallyHidden>
+        <InternalSelect>
+          {children}
+        </InternalSelect>
+      </label>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: inline-block;
+  font-family: "Roboto", sans-serif;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 2px;
+
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black}
+  }
+
+  position: relative;
+`;
+
+const SelectedWrapper = styled.div`
+  display: inline-flex;
+  column-gap: 24px;
+  align-items: center;
+  padding: 12px 16px;
+`;
+
+const InternalSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
 
 export default Select;
